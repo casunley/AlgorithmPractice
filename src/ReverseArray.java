d0a1901e2d4bd98ee05ae25b68ddec12025a037d
Curtis Sunley <casunleyjr@gmail.com>
2016-12-15 19:26:04 -0500

    Reverse array and reverse string implemented

--- src/ReverseArray.java
+++ src/ReverseArray.java
@@ -1,4 +1,27 @@
-
 public class ReverseArray {
+	public static int[] reverseArray(int[] arr){
+		
+		for(int i = 0; i < arr.length/2; i++){
+			int temp = arr[i];
+			arr[i] = arr[arr.length-i-1];
+			arr[arr.length-i-1]=temp;
+		}
+		
+		return arr;
+		
+	}
+	public static void main(String[] args){
+		int[] array = {1,2,3,4,5};
+		System.out.println("Normal: ");
+		for(int i = 0; i < array.length; i++){
+			System.out.print(array[i] + " ");
+		}
+		reverseArray(array);
+		System.out.println("\n" + "Reversed: ");
+		
+		for(int j = 0; j < array.length; j++){
+			System.out.print(array[j] + " ");
+		}
+	}
 
 }
--- src/ReverseString.java
+++ src/ReverseString.java
@@ -1,4 +1,25 @@
-
 public class ReverseString {
+	
+	public static StringBuilder reverseString(String string){
+		
+		StringBuilder sb = new StringBuilder(string);
+		return sb.reverse();
+	}
+	
+	public static String reverseStringNoAPI(String string){
+		String temp = ""; 
+		for(int i = string.length(); i > 0; i--){
+		temp += string.charAt(i-1);
+		}		
+		
+		return temp;
+	}
+	
+	public static void main(String[] args){
+		String string = ":D";
+		
+		System.out.println(reverseString(string));
+		System.out.println(reverseStringNoAPI(string));
+	}
 
 }
